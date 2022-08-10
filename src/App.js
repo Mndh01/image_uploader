9be7b6ad57e5900a51d5767237e8902e2487130e
MnDh01 <altair.2000nh@icloud.com>
2022-08-10 20:40:01 +0300

    uploaded photos appear in page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,71 @@
 import './App.css';
-import { useState } from 'react';
-import { storage } from './firebase';
-import { ref, uploadBytes } from 'firebase/storage';
+import { useEffect, useState } from 'react';
+import { storage, database, dbref } from './firebase';
+import { ref, getDownloadURL, uploadBytesResumable} from 'firebase/storage';
 import { v4 } from 'uuid';
+import { onValue, set } from 'firebase/database';
+
 
 function App() {
-  const [imageUpload, setImageUpload] = useState(null);
-  
-  const uploadImage = () => {
-    if (imageUpload == null) return;
-    const imageRef = ref(storage, `${imageUpload.name + v4()}`);
-    uploadBytes(imageRef, imageUpload).then(() => {
-      alert("Image uploaded");
-    })
-  };
+  const [image, setImage] = useState(null);
+  const [images, setImages] = useState([]);
 
   const ValidateInput = (event) => {
+    const fileInput = document.getElementById('file');
+    const filePath = fileInput.value;
 
-      let fileInput = document.getElementById('file');
-      let filePath = fileInput.value;
-
-      const allowedExtensions = /(\.jpg|\.jpeg|\.png)$/i;
-       
-      if (!allowedExtensions.exec(filePath)) {
-          alert('Only .jpg /.jpeg and .png types are allowed');
-          fileInput.value = '';
-          return false;
-      }
-      else
-      {
-        setImageUpload(event.target.files[0])
-        alert("Image ready for uploading");
-      }
+    const allowedExtensions = /(\.jpg|\.jpeg|\.png)$/i;
+     
+    if (!allowedExtensions.exec(filePath)) {
+        alert('Only .jpg /.jpeg and .png types are allowed');
+        fileInput.value = '';
+        return false;
+    }
+    else
+    {
+      setImage(event.target.files[0]);
+      alert("Image ready for uploading");
+    }
   };
 
+  const uploadImage = () => {
+    if (image == null) return;
+    
+    const nameAppend = v4();
+    const imageRef = ref(storage, `images/${image.name + nameAppend}`);
+    const uploadTask = uploadBytesResumable(imageRef, image);
+    
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {},
+      error => {
+        console.log(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(url => {
+          writeUrlToDatabase(url)
+          alert("Image uploaded successfully!");
+        })
+      }
+    );  
+  };
+
+  const writeUrlToDatabase = (url) => {
+    const uuid = v4();
+    set(dbref(database, `${uuid}`), {
+      url
+    });
+  }
+  
+  useEffect(() => {
+    onValue(dbref(database), (snapshot) => {
+      const data = snapshot.val();
+      console.log("data ",  data);
+      console.log("object ", Object.values(data));
+      setImages(Object.values(data));
+    });
+  }, []);
+  
   return (
     <div className="App">
       <h1 className='Title'>Image Uploader</h1>
@@ -44,6 +76,12 @@ function App() {
         <button id='upload' onClick={uploadImage}>Upload Image</button>
         <label htmlFor='upload'>Upload Image</label>
       </div>
+
+      <div className='imgContainer'>
+        {images.map(({url}) => {
+          return (<img src={url} key={url} />)
+        })};
+        </div>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -3,10 +3,12 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getStorage } from "firebase/storage";
+import { getDatabase, ref } from "firebase/database";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDMVpxBmf0sJgJfCMJ90s1bI_bWs73u6DM",
   authDomain: "imageuploader-be96a.firebaseapp.com",
+  databaseURL: "https://imageuploader-be96a-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "imageuploader-be96a",
   storageBucket: "imageuploader-be96a.appspot.com",
   messagingSenderId: "299168764476",
@@ -15,4 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const dbref = ref;
 export const storage = getStorage(app);
+export const database = getDatabase(app);
